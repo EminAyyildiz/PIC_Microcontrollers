fe44da072df91f76492d8994e518ad7545b28bd3
Emin Ayyıldız <115374624+EminAyyildiz@users.noreply.github.com>
2023-09-24 13:41:22 +0300

    Add files via upload

---
+++ TIMER0_INTERRUPT/TIMER0_KES.X/TIMER0_KES.c
@@ -0,0 +1,47 @@
+/*
+ * File:   timer0_uygulamasi.c
+ * Author: User
+ *
+ * Created on 30 October 2019, 12:50 PM
+ */
+
+
+#include <xc.h>
+
+#pragma config FOSC = XT        // Oscillator Selection bits (RC oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer (WDT enabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (Power-up Timer is disabled)
+#pragma config CP = OFF         // Code Protection bit (Code protection disabled)
+
+#define __XTAL_FREQ 4000000
+
+int sayac;
+
+void __interrupt() KESME(void){
+    
+    if(T0IF){
+        
+        sayac++;
+        if(sayac==10){
+            sayac=0;
+            RB0=RB0 ^ 1; // 0000 0001
+        }
+        TMR0=61;
+        T0IF=0;
+    }
+    
+}
+
+void main(void) {
+    
+    GIE=1;
+    T0IE=1;
+    OPTION_REG=0x07; //0b 0000 0111
+    TRISB=0x00;
+    TMR0=61;
+    sayac=0;
+    PORTB=0;
+    
+    while(1){}
+    
+}
---
+++ TIMER0_INTERRUPT/TIMER0_KES.X/build/default/production/TIMER0_KES.p1.d
@@ -0,0 +1,2 @@
+build/default/production/TIMER0_KES.p1:  \
+TIMER0_KES.c 
